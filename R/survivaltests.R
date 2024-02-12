d3f460e8248dbc4475d9723798dce0d824c0f2bd
sfweller84 <sfweller425@gmail.com>
2024-02-12 11:41:32 -0800

    Fix survival test in the package

--- R/survivaltests.R
+++ R/survivaltests.R
@@ -1,5 +1,6 @@
-survfit <- function(data = bmt)
+survfit <- function()
 {
+  bmt <- system.file("extdata", "bonemarrowdat.csv", package = "rpacktests")
   bmt.sdiff <- survdiff(Surv(bmt$T2, bmt$d3) ~ bmt$g, data = bmt)
   bmt.sdiff
 }
