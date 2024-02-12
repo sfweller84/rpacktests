a58587bfbcd311c69c4a37742024b90f101fd81d
sfweller84 <sfweller425@gmail.com>
2024-02-12 13:27:50 -0800

    Add testthat.R code

---
+++ tests/testthat.R
@@ -0,0 +1,24 @@
+library(testthat)
+library(rpacktests)
+
+# Load r-recommended libraries.
+
+library(KernSmooth)
+library(MASS)
+library(Matrix)
+library(boot)
+library(class)
+library(cluster)
+library(foreign)
+library(mgcv)
+library(nlme)
+library(rpart)
+library(spatial)
+library(survival)
+library(utils)
+library(nnet)
+
+test_check("rpacktests")
+
+
+
