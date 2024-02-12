232baa4bf03f07a953dc48f3ef0b7d935d845219
sfweller84 <sfweller425@gmail.com>
2024-02-12 13:53:02 -0800

    Add description filen and update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,23 +1,6 @@
 library(testthat)
 library(rpacktests)
 
-# Load r-recommended libraries.
-
-library(KernSmooth)
-library(MASS)
-library(Matrix)
-library(boot)
-library(class)
-library(cluster)
-library(foreign)
-library(mgcv)
-library(nlme)
-library(rpart)
-library(spatial)
-library(survival)
-library(utils)
-library(nnet)
-
 test_check("rpacktests")
 
 
