d693508ae485974c07ce1415dc0b6d439a537197
sfweller84 <sfweller425@gmail.com>
2024-02-18 16:54:36 -0800

    Add missing parenthesis to read.table call.

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,7 @@ library(testthat)
 library(rpacktests)
 
 # Read in survival package test data.
-bmt <- read.table(paste(getwd(), "../data-raw/bonemarrowdat.csv", header=TRUE, sep = ",", quote = "", row.names=NULL)
+bmt <- read.table(paste(getwd(), "/../data-raw/bonemarrowdat.csv", sep = ""), header=TRUE, sep = ",", quote = "", row.names=NULL)
 
 test_check("rpacktests")
 
